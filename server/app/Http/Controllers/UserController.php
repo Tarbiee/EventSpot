f55cc52af1b2125a84a7a55572545c8eee471dab
Tarbiee <dorcusstaby@gmail.com>
2024-02-27 02:49:45 +0300

    Login post request

--- server/app/Http/Controllers/UserController.php
+++ server/app/Http/Controllers/UserController.php
@@ -18,5 +18,18 @@ class UserController extends Controller
         $user->save();
         return $user;
     }
-    
+    function login(Request $req)
+    {   
+        $user = User::where('email', $req->input('email'))->first();
+        if (!$user) {
+        return response()->json(['error' => "User not found"], 404);
+    }
+    // Check if the password matches
+    if (!Hash::check($req->input('password'), $user->password)) {
+        return response()->json(['error' => "Email or password is not matched"], 401);
+    }
+    // If both email and password are correct, return user data
+    return $user;
+    }
+
 }
