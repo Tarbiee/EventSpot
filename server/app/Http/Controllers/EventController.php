9a146586213bdfb7df6e11f61cf8fe976ffa70ee
Tarbiee <dorcusstaby@gmail.com>
2024-02-27 12:24:06 +0300

    Add event API

---
+++ server/app/Http/Controllers/EventController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Event;
+
+class EventController extends Controller
+{
+    //
+    function addEvent(Request $req)
+    {
+        $event = new Event;
+        $event->event_name=$req->input('event_name');
+        $event->event_description=$req->input('event_description');
+        $event->event_date=$req->input('event_date');
+        $event->event_location=$req->input('event_location');
+        $event->event_organizer=$req->input('event_organizer');
+        $event->event_category=$req->input('event_category');
+        $event->event_capacity=$req->input('event_capacity');
+        $event->event_status=$req->input('event_status');
+        $event->ticket_type=$req->input('ticket_type');
+        $event->ticket_price=$req->input('ticket_price');
+        $event->available_tickets=$req->input('available_tickets');
+        $event->event_image=$req->file('event_image') ->store('uploads');
+        $event->save();
+        return $event;
+        
+    }
+}
--- server/routes/api.php
+++ server/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\EventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('register',[UserController::class,'register']);
-Route::post('login',[UserController::class,'login']);//call login function inside usercontroller
+Route::post('login',[UserController::class,'login']);
+Route::post('add_event',[EventController::class,'addEvent']);
